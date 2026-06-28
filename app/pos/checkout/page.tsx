4dd5fbb69143179529917a2cf9ecab6e1f28796e
Mpictdev <taufiksip1722@gmail.com>
2026-06-28 21:45:25 +0700

    aa

--- app/pos/checkout/page.tsx
+++ app/pos/checkout/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { usePosStore } from '@/lib/store/usePosStore';
 import { processTransaction } from '@/app/actions/transaction';

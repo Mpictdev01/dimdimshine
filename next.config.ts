44e5502d8c0913cc1c363fa9ab110e3e42a1aa4d
Mpictdev <taufikhida705@gmail.com>
2026-07-02 12:00:35 +0700

    pwa fix

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,6 @@ const withPWA = withPWAInit({
   cacheOnFrontEndNav: true,
   aggressiveFrontEndNavCaching: true,
   reloadOnOnline: true,
-  swcMinify: true,
   disable: false, // Set false to enable PWA in dev for testing. Otherwise process.env.NODE_ENV === "development"
   workboxOptions: {
     disableDevLogs: true,
